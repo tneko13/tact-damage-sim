be577bf6e2e9da79e7b172d5b148e2e4367116c0
tneko13 <ignbys@gmail.com>
2025-10-26 03:04:11 +0900

    test

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
+  const viteLogo = new URL('/vite.svg', import.meta.url).href
   import Counter from './lib/Counter.svelte'
 </script>
 
@@ -13,6 +13,22 @@
     font-weight: 700;
     font-size: 14px;
     box-shadow: 0 2px 6px rgba(0,0,0,0.15);
+  }
+
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
   }
 </style>
 
@@ -43,21 +59,3 @@
     Click on the Vite and Svelte logos to learn more
   </p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
